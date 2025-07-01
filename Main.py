f82925763df0e9947775212e56b5e5b498a3fc4c
jbaker454 <jbaker769076@gmail.com>
2025-07-01 18:16:42 -0500

    added button

--- Main.py
+++ Main.py
@@ -1,4 +1,12 @@
 import Data_Handler
 import UI_Handler
 
-UI_Handler.window.mainloop()
+def main():
+    UI_Handler.window.mainloop()
+
+    UI_Handler.EventTest.bind("<Button-1>", lambda event, newMenu="mainMenuFrame": testFuntion())
+
+def testFuntion():
+    pass
+
+main()
--- UI_Handler.py
+++ UI_Handler.py
@@ -1,4 +1,27 @@
 import TKinter_Wrapper as widgets
 
+
+def initilizeConfigurations():
+    widgets.Config("Purple",{"bg":"#634ed0", "highlightthickness":2,"highlightbackground":"#000000", "borderwidth":2})
+    widgets.Config("DarkGrey",{"bg":"#3d3d5c","highlightthickness":2,"highlightbackground":"#000000","borderwidth":4})
+    widgets.Config("ScrollingFrameBlue",{"bg":"#000099","highlightthickness":2,"highlightbackground":"#000000","borderwidth":4})
+    widgets.Config("ScrollingFrameBlue2",{"bg":"#000099","highlightthickness":2,"highlightbackground":"#000000","borderwidth":4})
+    widgets.Config("LightBlue",{"bg":"#3377ff","highlightthickness":2,"highlightbackground":"#000000","borderwidth":4})
+    widgets.Config("Yellow",{"bg":"#b0f943","highlightthickness":2,"highlightbackground":"#000000","borderwidth":2})
+    widgets.Config("DarkBlue",{"bg":"#000066","highlightthickness":2,"highlightbackground":"#000000","borderwidth":2})
+    widgets.Config("CardRed",{"bg":"#e60000","highlightthickness":2,"highlightbackground":"#000000","borderwidth":2})
+    widgets.Config("CardGreen",{"bg":"#00e600","highlightthickness":2,"highlightbackground":"#000000","borderwidth":2})
+    widgets.Config("CardLightBlue",{"bg":"#00e6e6","highlightthickness":2,"highlightbackground":"#000000","borderwidth":2})
+    widgets.Config("CardGrey",{"bg":"#8c8c8c","highlightthickness":2,"highlightbackground":"#000000","borderwidth":1})
+
+initilizeConfigurations()
+
 window = widgets.Widget.get_instance(widgets.Widget,"Wonkas Chocolate")
 window = widgets.Window(name="Bible CardDeck Simulation", size=[1000,600])
+
+EventTest = widgets.Widget.get_instance(widgets.Widget,"EventTest")
+EventTest = widgets.CustomButton(
+           name="EventTest", parent=window, size=[10,2], 
+           config=widgets.Config.getCustomConfigByName(widgets.Config,"Yellow"), **{"text":"back"})
+EventTest.place(x=730,y=485)
+
