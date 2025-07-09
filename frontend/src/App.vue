bfeadf78cc2580fd86f9f42ccfc397272347f2df
jbaker454 <jbaker769076@gmail.com>
2025-07-08 19:00:24 -0500

    added pages

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/Inventory">Inventory</router-link> |
+      <router-link to="/Orders">Orders</router-link> |
+      <router-link to="/Processes">Processes</router-link> |
+      <router-link to="/Shipments">Shipments</router-link>
+    </nav>
+    <router-view />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
---
+++ frontend/src/router/index.ts
@@ -0,0 +1,22 @@
+// src/router/index.ts
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/pages/Home.vue'
+import Inventory from '@/pages/Inventory.vue'
+import Shipments from '@/pages/Shipments.vue'
+import Processes from '@/pages/Processes.vue'
+import Orders from '@/pages/Orders.vue'
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/Inventory', component: Inventory },
+  { path: '/Shipments', component: Shipments },
+  { path: '/Processes', component: Processes },
+  { path: '/Orders', component: Orders }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
