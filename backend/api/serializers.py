28e024e612fb6312ea23467182f569657601070b
Madison Hauptmann <mhauptmann@ivytech.edu>
2025-07-10 20:11:07 -0400

    Added more urls and models

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import User, Product
+from .models import User, Product, Item, Shipment, Process, Order
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -13,4 +13,24 @@ class BookSerializer(serializers.ModelSerializer):
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
+        fields = '__all__'
+
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = '__all__'
+
+class ShipmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Shipment
+        fields = '__all__'
+
+class ProcessSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Process
+        fields = '__all__'
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
         fields = '__all__'
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,29 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status, viewsets
-from .models import User, Product
-from .serializers import UserSerializer, BookSerializer, ProductSerializer
+from .models import User, Product, Item, Shipment, Process, Order
+from .serializers import UserSerializer, BookSerializer, ProductSerializer, ItemSerializer, ShipmentSerializer, ProcessSerializer, OrderSerializer
 from django.shortcuts import get_object_or_404
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+class ShipmentViewSet(viewsets.ModelViewSet):
+    queryset = Shipment.objects.all()
+    serializer_class = ShipmentSerializer
+
+class ProcessViewSet(viewsets.ModelViewSet):
+    queryset = Process.objects.all()
+    serializer_class = ProcessSerializer
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
 
 @api_view(['GET', 'POST'])
 def user_list(request):
